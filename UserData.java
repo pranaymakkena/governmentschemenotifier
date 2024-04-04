ca3f352d7b482ef7c84646ba20a26388870dbb2e
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-04 11:44:27 +0530

    Update and rename terminal_msg.java to UserData.java

---
+++ UserData.java
@@ -0,0 +1,17 @@
+public class UserData {
+    private int age;
+    private double income;
+
+    public UserData(int age, double income) {
+        this.age = age;
+        this.income = income;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+}
