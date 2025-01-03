b4dd20a0d34ddcd497592298bbf84dc2b1f682dd
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2025-01-03 13:47:03 +0530

    Update RecommendationPage.java

--- RecommendationPage.java
+++ RecommendationPage.java
@@ -8,11 +8,11 @@ public class RecommendationPage {
             // Check eligibility for Scholarship based on income
             if (userData.getIncome() < 200000) {
                 System.out.println("User with Age: " + userData.getAge() +
-                                   ", Income: ₹" + userData.getIncome() +
+                                   ", Income: " + userData.getIncome() +
                                    " is eligible for Scholarship.");
             } else {
                 System.out.println("User with Age: " + userData.getAge() +
-                                   ", Income: ₹" + userData.getIncome() +
+                                   ", Income: " + userData.getIncome() +
                                    " is not eligible for Scholarship.");
             }
         }
